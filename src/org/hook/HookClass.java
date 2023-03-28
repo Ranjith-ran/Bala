a97a116b34cb66ae2e317f8feaed77f44db75877
Ranjith-ran <ranjithv411@gmail.com>
2023-03-28 20:07:47 +0530

    one

---
+++ src/org/hook/HookClass.java
@@ -0,0 +1,15 @@
+package org.hook;
+
+public class HookClass {
+	public static void main(String[] args) {
+		int a = 100;
+
+		int b = 200;
+
+		int c = a + b;
+
+		System.out.println(c);
+
+	}
+
+}
